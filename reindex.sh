4c95ba92062b7483bb1478d2838e0fc6e6077df4
nejdetckenobi <nejdetyucesoy@gmail.com>
2024-02-11 16:00:01 +0300

    attempt

---
+++ reindex.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "# Index" > docs/index.md
+echo "" >> docs/index.md
+find docs -name '*.md' -and ! -name 'index.md' | cut -d . -f 1 | cut -d / -f 2 | xargs -I% echo "- (%)[%.html]" >> docs/index.md
